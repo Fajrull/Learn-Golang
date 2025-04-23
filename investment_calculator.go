63b241d208fbbd84cae5e74bf7250552d21a953d
Muhammad Fajrul Khaq <fajrulm05@gmail.com>
2025-04-23 12:18:20 +0700

    add investment-calculator

---
+++ investment_calculator.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main(){
+	const inflationRate = 2.5
+	var investmentAmount float64
+	expectedReturnRate := 5.5
+	var years float64
+
+	fmt.Print("Investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
+
+	fmt.Print("Future value: ")
+	fmt.Println(futureValue)
+
+	fmt.Print("Future real value: ")
+	fmt.Println(futureRealValue)
+}
